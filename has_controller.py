048fefa7bac5c861208a0ca5ce7b9311a1ba5a29
navid shariaty <navid.1378.1384@gmail.com>
2020-09-18 08:00:45 +0430

    add documents and config.yaml file

--- has_controller.py
+++ has_controller.py
@@ -5,7 +5,7 @@ import has_view
 
 def parse_args():
     args = argparse.ArgumentParser()
-    args.add_argument("-c", "--config", dest="config", required=True, help="path to config file.(in case of multiple config files, last one will get ahead of the rest.)")
+    args.add_argument("-c", "--config", dest="config", required=True, help="absolute path to config file.(in case of multiple config files, last one will get ahead of the rest.)")
     return args.parse_args()
 
 
@@ -15,7 +15,8 @@ def main():
     has_view.View(config_content, state, only_view_on_errors=True)
     if not state:
         exit(1)
-    print(config_content)
+    hesabies, state = hesabs.load_hesabi_bodies(config_content.get("hesabi_path"))
+    has_view.View(hesabies, state, only_view_on_errors=True)
 
 
 if __name__ == '__main__':
--- hesabs.py
+++ hesabs.py
@@ -1,13 +1,25 @@
+"""
+in this module we will parse two kind of files that are too important for us
+1) config file
+    name of this config file is passed to us in controller with "-c" or "--config"
+    this file contains:
+        :param:str hesabies_path(required)  =>  absolute path to directory that contains hesabi files
+        :param:list sources(required)       =>  a list of sources to read from with their details
+        :param:list actions(required)       =>  a list of actions with their details
+        :param:list pipe_type(required)     =>  a list with only one element, specifies the pipeline type to find out if actions should get triggered or not
+2) hesabi files
+each hesabi is a yaml file with contents of
+"""
 import os
-from staticconf.loaders import yaml_loader
+from staticconf.loader import yaml_loader
 
 
 def load_config(config_path):
-    if os.path.isfile(config_path):
+    if not os.path.isfile(config_path):
         return "No such file {}".format(config_path), False
     try:
         content = yaml_loader(config_path)
-        return content
+        return content, True
     except:
         return "Could not load file {}".format(config_path), False
 
@@ -22,6 +34,8 @@ def load_hesabi_body(hesabi_path):
 
 
 def load_hesabi_bodies(hesabies_path):
+    if not hesabies_path:
+        return "hesabies_path should have a value", False
     if not os.path.isdir(hesabies_path):
         return "No such directory {}".format(hesabies_path), False
     hesabi_bodies = dict()
